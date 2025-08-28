fc9cef7c6c456d011a75ebc844fde380ea19715e
Nimish Patel <nimishpatel19@gmail.com>
2025-08-28 13:02:00 +0530

    added config for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['picsum.photos', 'd2ei5bvt6t47qt.cloudfront.net'],
+        domains: ['picsum.photos',
+            'd2ei5bvt6t47qt.cloudfront.net',
+            'd2pt7ajyubzgoz.cloudfront.net',
+            'admin.damgoodfish.com'
+        ],
     }
 };
 
