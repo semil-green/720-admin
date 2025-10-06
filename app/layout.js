7ec7314d5995ecf37536ac000f36f414bca36d0e
semil21 <you@example.com>
2025-10-06 17:29:56 +0530

    fixed scroll issue in static page

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-scroll`}>
         <ReduxProvider>
           {children}
         </ReduxProvider>
